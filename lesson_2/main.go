b546c99be45d90e33a84f1396753c0ee67dd9618
revolverking927 <skeyondre12@gmail.com>
2025-07-14 22:27:24 -0500

    Lesson 2 completed

--- lesson_2/main.go
+++ lesson_2/main.go
@@ -3,5 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World")
+	//String variables
+	var nameOne string = "Alice"
+	var nameTwo = "Bob"
+	var nameThree string
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameOne = "Charlie"
+	nameThree = "David"
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameFour := "Eve"
+	fmt.Println(nameFour)
+
+	// Integer variables
+	var ageOne int = 30
+	var ageTwo = 25
+	ageThree := 20
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// Bits and memory
+	var ageFour int8 = 22
+	var ageFive int16 = 28912
+	var ageSix uint16 = 256
+	fmt.Println(ageFour, ageFive, ageSix)
+
+	var scoreOne float32 = 95.5
+	var scoreTwo float64 = 88.75
+	fmt.Println(scoreOne, scoreTwo)
 }
